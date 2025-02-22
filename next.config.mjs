4938f99eea0e201a8f5d32209a08bbf102279abc
Daniel Le <ledaniel@MacBook-Air-1281.local>
2025-02-21 19:36:32 -0800

    Merge gh-pages into main and update config files

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,12 @@ let userConfig = undefined
 try {
   userConfig = await import('./v0-user-next.config')
 } catch (e) {
-  // ignore error
+  // ignore error if the user config file doesn't exist
 }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', 
   eslint: {
     ignoreDuringBuilds: true,
   },
